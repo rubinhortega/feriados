2211f97da3144db4ad0e837cbb3c52b5f09fb722
Rubens Ortega <91102160+rubinhortega@users.noreply.github.com>
2022-10-26 19:29:05 -0300

    Initial commit

---
+++ core/__pycache__/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+
+
+class FeriadoForm(forms.Form):
+
+    nome = forms.CharField(max_length=50)
+
+    dia = forms.IntegerField()
+
+    mes = forms.IntegerField()
+
+
+
+    def clean_nome(self):
+
+        nome = self.clean_nome[nome]
+
+        return nome.upper
---
+++ core/models.py
@@ -0,0 +1,17 @@
+from tabnanny import verbose
+from django.db import models
+
+
+class FeriadoModel(models.Model):
+    nome = models.CharField('Feriado', max_length=50)
+    dia = models.IntegerField('Data')
+    mes = models.IntegerField('Mes')
+    modificado_em = models.DateTimeField(verbose_name='modificado em', auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.nome + '=> ' + str(self.dia)
+    
+    class Meta:
+        verbose_name = 'Feriado'
+        verbose_name_plural = 'Feriados'
+        ordering = ('mes','-dia')
